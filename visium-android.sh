8f9cdc2a1e1a65db227af83d8acf8ec4ebb7cdea
CAHYA JULIAN <cahya.julian@bankmandiri.co.id>
2024-02-01 16:21:23 +0700

    download manually script-update

--- visium-android.sh
+++ visium-android.sh
@@ -11,7 +11,7 @@ if ! command -v jq &> /dev/null; then
     apt-get install -y jq
 
     # Check installation success
-    if command -v jq &> /dev/null; then
+    if command -v jq then
         echo "jq has been successfully installed."
     else
         echo "Failed to install jq. Please install it manually."
@@ -19,8 +19,6 @@ if ! command -v jq &> /dev/null; then
 else
     echo "jq is already installed."
 fi
-isAvail=$(command -v jq)
-echo "$isAvail"
 echo "step 1 --> DONE <--" 
 
 # step 2: get all devide id of Android
