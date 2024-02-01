765ae6c8875ac93cc4bedebb849a7c5c53fc5e65
CAHYA JULIAN <cahya.julian@bankmandiri.co.id>
2024-02-01 17:36:28 +0700

    install brew script

--- visium-android.sh
+++ visium-android.sh
@@ -6,12 +6,6 @@ set -x
 if ! command -v jq &> /dev/null; then
     echo "jq is not installed. Installing..."
 
-    # Check if Homebrew is installed
-    if ! command -v brew &> /dev/null; then
-        echo "Homebrew is not installed. Installing..."
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    fi
-        
     # Install jq using Homebrew
     brew install jq
 
